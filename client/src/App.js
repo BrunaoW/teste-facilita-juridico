4fc043a003471c7af54ed651f23db9837cbdb142
Bruno Wilson <brunowilson4@gmail.com>
2024-03-06 15:27:53 -0300

    Adiciona frontend

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Header } from 'semantic-ui-react'
+
+function App() {
+  return (
+    <div className="App">
+      <Header as='h1'>Acompanha Limpeza</Header>
+    </div>
+  );
+}
+
+export default App;
